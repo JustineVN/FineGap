a7f26f7935f0d499644f7de6a7b2d8ec0564ee49
JustineVN <84462284+JustineVN@users.noreply.github.com>
2023-07-09 20:22:27 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -1,4 +1,4 @@
-const { createApp } = Vue;
+/*const { createApp } = Vue;
 
 createApp({
   data() {
@@ -9,7 +9,7 @@ createApp({
       url: 'http://mcerda.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
       error: false,
       cargando: true,
-      /*atributos para el guardar los valores del formulario */
+      /*atributos para el guardar los valores del formulario 
       id: 0,
       nombre: "",
       imagen: "",
@@ -67,5 +67,74 @@ createApp({
   },
   created() {
     this.fetchData(this.url);
+  },
+}).mount('#app');*/
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      productos: [],
+      url: 'https://solmendel.pythonanywhere.com/productos',
+      error: false,
+      cargando: true,
+      id: 0,
+      nombre: "",
+      imagen: "",
+      stock: 0,
+      precio: 0,
+    };
+  },
+  methods: {
+    fetchData() {
+      fetch(this.url)
+        .then(response => response.json())
+        .then(data => {
+          this.productos = data;
+          this.cargando = false;
+        })
+        .catch(err => {
+          console.error(err);
+          this.error = true;
+        });
+    },
+    eliminar(producto) {
+      const url = this.url + '/' + producto;
+      var options = {
+        method: 'DELETE',
+      };
+      fetch(url, options)
+        .then(res => res.text())
+        .then(res => {
+          location.reload();
+        });
+    },
+    grabar() {
+      let producto = {
+        nombre: this.nombre,
+        precio: this.precio,
+        stock: this.stock,
+        imagen: this.imagen,
+      };
+      var options = {
+        body: JSON.stringify(producto),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        redirect: 'follow',
+      };
+      fetch(this.url, options)
+        .then(function () {
+          alert("Registro grabado");
+          window.location.href = "./productos.html";
+        })
+        .catch(err => {
+          console.error(err);
+          alert("Error al Grabar");
+        });
+    },
+  },
+  created() {
+    this.fetchData();
   },
 }).mount('#app');
